3b9bd2462ca2d8d1933c19da146e834538659e8a
justintsugranes <justintsugranes@proton.me>
2022-12-19 15:21:12 -0500

    add styles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -7,6 +8,9 @@ const app = express()
 // enable body parser so we can take in body data
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
